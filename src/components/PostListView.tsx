f2ef47e2de9af49763f31cce0c412cc790aa6c6c
Takusea <takusea826@gmail.com>
2025-12-30 23:59:49 +0900

    refactor: コンポーネントを整理

---
+++ src/components/PostListView.tsx
@@ -0,0 +1,32 @@
+import { A } from "@solidjs/router";
+import { For } from "solid-js";
+import styles from "./PostListView.module.css";
+import type { PostMetadata } from "~/type/postmetadata";
+
+type Props = {
+	posts: PostMetadata[];
+};
+
+const PostListView = (props: Props) => {
+	return (
+		<ul class={styles.posts}>
+			<For each={props.posts}>
+				{(post) => (
+					<li>
+						<A class={styles.post} href={post.slug}>
+							<div class={styles["post-title"]}>{post.title}</div>
+							<div class={styles["post-metadata"]}>
+								<time>{post.date}</time>
+								<For each={post.tags}>
+									{(tag) => <div class={styles["post-tag"]}>{tag}</div>}
+								</For>
+							</div>
+						</A>
+					</li>
+				)}
+			</For>
+		</ul>
+	);
+};
+
+export { PostListView };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createAsync } from "@solidjs/router";
-import { For } from "solid-js";
 import { getPosts } from "~/lib/posts";
-import { A } from "@solidjs/router";
 import styles from "./index.module.css";
+import { PostListView } from "~/components/PostListView";
+import { Show } from "solid-js";
 
 export default function Index() {
 	const posts = createAsync(() => getPosts());
@@ -11,25 +11,12 @@ export default function Index() {
 		<main class={styles.main}>
 			<h1 class={styles.title}>たくしいの雑記</h1>
 			<p>備忘録と痕跡残し</p>
-			<ul class={styles.articles}>
-				<For each={posts()}>
-					{(post) => (
-						<li>
-							<A class={styles.article} href={post.data.frontmatter.slug}>
-								<div class={styles["article-title"]}>
-									{post.data.frontmatter.title}
-								</div>
-								<div class={styles["article-metadata"]}>
-									<time>{post.data.frontmatter.date}</time>
-									<For each={post.data.frontmatter.tags}>
-										{(tag) => <div class={styles["article-tag"]}>{tag}</div>}
-									</For>
-								</div>
-							</A>
-						</li>
-					)}
-				</For>
-			</ul>
+			<Show
+				when={posts()?.map((post) => post.data.frontmatter)}
+				fallback={<p>Loading...</p>}
+			>
+				{(postMetadatas) => <PostListView posts={postMetadatas()} />}
+			</Show>
 		</main>
 	);
 }
