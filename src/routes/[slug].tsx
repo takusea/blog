78ce1067aa743289a43e1c941b0a82e01351aff3
Takusea <takusea826@gmail.com>
2026-01-05 22:38:22 +0900

    feat: ページにMetaタグを設定

--- src/routes/[slug].tsx
+++ src/routes/[slug].tsx
@@ -1,36 +1,61 @@
-import { createAsync, useParams } from "@solidjs/router";
-import { Show } from "solid-js";
+import { A, createAsync, useParams } from "@solidjs/router";
+import { createMemo, Show } from "solid-js";
 import { getPosts } from "~/lib/posts";
 import { TocView } from "~/components/TocView";
-import styles from "./slug.module.css";
+import styles from "./[slug].module.css";
 import { DocumentView } from "~/components/DocumentView";
 import { TagListView } from "~/components/TagListView";
+import { Link, Meta, MetaProvider, Title } from "@solidjs/meta";
 
 export default function BlogPost() {
 	const params = useParams();
 	const posts = createAsync(() => getPosts(), { deferStream: true });
+	const post = createMemo(() =>
+		posts()?.find((post) => post.data.frontmatter.slug === params.slug),
+	);
 
 	return (
-		<Show
-			when={posts()?.find((post) => post.data.frontmatter.slug === params.slug)}
-			fallback="error"
-		>
+		<Show when={post()} fallback="error">
 			{(post) => (
-				<main class={styles.main}>
-					<aside class={styles.side}>
-						<TocView toc={post().data.toc} />
-					</aside>
-					<article class={styles.article}>
-						<div class={styles.header}>
-							<h1 class={styles.title}>{post().data.frontmatter.title}</h1>
-							<time class={styles.date} datetime={post().data.frontmatter.date}>
-								{post().data.frontmatter.date}
-							</time>
-							<TagListView tags={post().data.frontmatter.tags} />
-						</div>
-						<DocumentView document={post().value} />
-					</article>
-				</main>
+				<MetaProvider>
+					<Title>{`${post().data.frontmatter.title} - たくしいの雑記`}</Title>
+					<Link
+						rel="canonical"
+						href={`https://takusea.com/blog/${post().data.frontmatter.slug}`}
+					/>
+					<Meta
+						property="og:title"
+						content={`${post().data.frontmatter.title} - たくしいの雑記`}
+					/>
+					<Meta property="og:type" content="article" />
+					<Meta
+						property="og:url"
+						content={`https://takusea.com/blog/${post().data.frontmatter.slug}`}
+					/>
+					<main class={styles.main}>
+						<aside class={styles.side}>
+							<TocView toc={post().data.toc} />
+						</aside>
+						<article class={styles.article}>
+							<div class={styles.header}>
+								<div class={styles.breadcrumbs}>
+									<div class={styles.breadcrumb}>
+										<A href="/">ホーム</A>
+									</div>
+								</div>
+								<h1 class={styles.title}>{post().data.frontmatter.title}</h1>
+								<time
+									class={styles.date}
+									datetime={post().data.frontmatter.date}
+								>
+									{post().data.frontmatter.date}
+								</time>
+								<TagListView tags={post().data.frontmatter.tags} />
+							</div>
+							<DocumentView document={post().value} />
+						</article>
+					</main>
+				</MetaProvider>
 			)}
 		</Show>
 	);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,20 +3,30 @@ import { getPosts } from "~/lib/posts";
 import styles from "./index.module.css";
 import { PostListView } from "~/components/PostListView";
 import { Show } from "solid-js";
+import { Link, Meta, MetaProvider, Title } from "@solidjs/meta";
+// import { SearchField } from "~/components/SearchField";
 
 export default function Index() {
 	const posts = createAsync(() => getPosts(), { deferStream: true });
 
 	return (
-		<main class={styles.main}>
-			<h1 class={styles.title}>たくしいの雑記</h1>
-			<p>備忘録と痕跡残し</p>
-			<Show
-				when={posts()?.map((post) => post.data.frontmatter)}
-				fallback={<p>Loading...</p>}
-			>
-				{(postMetadatas) => <PostListView posts={postMetadatas()} />}
-			</Show>
-		</main>
+		<MetaProvider>
+			<Title>"たくしいの雑記"</Title>
+			<Link rel="canonical" href="https://takusea.com/blog/" />
+			<Meta property="og:title" content="たくしいの雑記" />
+			<Meta property="og:type" content="website" />
+			<Meta property="og:url" content="https://takusea.com/blog/" />
+			<main class={styles.main}>
+				<h1 class={styles.title}>たくしいの雑記</h1>
+				<p>備忘録と痕跡残し</p>
+				{/* <SearchField /> */}
+				<Show
+					when={posts()?.map((post) => post.data.frontmatter)}
+					fallback={<p>Loading...</p>}
+				>
+					{(postMetadatas) => <PostListView posts={postMetadatas()} />}
+				</Show>
+			</main>
+		</MetaProvider>
 	);
 }
