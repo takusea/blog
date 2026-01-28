a1427ab443a71db57fd66b70b306c22896fde99e
Takusea <takusea826@gmail.com>
2026-01-28 19:10:59 +0900

    feat: サイトマップ生成機能を追加

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,16 @@
+import fs from "node:fs";
+import path from "node:path";
 import { defineConfig } from "@solidjs/start/config";
+import solidStartSiteMapPlugin from "solid-start-sitemap";
+
+const getPostSlugs = () => {
+	return fs
+		.readdirSync(path.join(process.cwd(), "public/posts"), {
+			withFileTypes: true,
+		})
+		.filter((entry) => entry.isFile() && entry.name.endsWith(".md"))
+		.map((file) => path.parse(file.name).name);
+};
 
 export default defineConfig({
 	server: {
@@ -7,5 +19,16 @@ export default defineConfig({
 		prerender: {
 			crawlLinks: true,
 		},
+	},
+	vite: {
+		plugins: [
+			solidStartSiteMapPlugin({
+				hostname: "https://blog.takusea.com",
+				replaceRouteParams: {
+					":slug": getPostSlugs(),
+				},
+				limit: 5000,
+			}),
+		],
 	},
 });
